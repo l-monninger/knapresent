c38d57973479f4798e3452201f32d5db1abd5836
Liam Monninger <l-monninger@Liams-MacBook-Air.local>
2021-12-21 16:50:33 -0800

    Updating buil.

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 import { LoginContext, LoginProvider } from './Login/LoginProvider';
 import { Xmas2021 } from './Presents/Xmas2021';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <LoginProvider>
-        <Xmas2021/>
+        <BrowserRouter>
+          <Routes>
+            <Route path="xmas-2021" element={<Xmas2021/>}/>
+          </Routes>
+        </BrowserRouter>
       </LoginProvider>
     </div>
   );
